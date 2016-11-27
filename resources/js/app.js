0e751462aca4b67c87b7248c75ee009d78ed6f96
Young Jun Park <june0313@gmail.com>
2016-11-27 19:02:50 +0900

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+	// 숫자 목록 초기화
+	initializeNumbers();
+
+	// 컴포넌트에 이벤트 바인딩
+	bindEvents();
+});
+
+function initializeNumbers() {
+	var operands = $("#operands"), answers = $("#answers"), numberCount;
+	operands.empty();
+	answers.empty();
+	numberCount = $("#count-select option:selected").val();
+
+	for (var i = 0; i < numberCount; i++) {
+		// 1 ~ 9 까지의 랜덤한 숫자 생성
+		var number = getRandomNumber(1, 9);
+		operands.append("<div class='operand'>" + number + "</div>");
+		$(".operand").fadeIn("slow").css("display","inline-block");
+
+		// 연산자(+) 출력
+		if (i !== numberCount - 1) {
+			operands.append("<span class='operator'>+</span class='ui label'>");
+		}
+	}
+
+	// 총 합이 19를 넘어가면 다시 초기화 한다.
+	var answer = calculateAnswer();
+	if (answer > 19) {
+		initializeNumbers();
+	}
+}
+
+function getRandomNumber(min, max) {
+	max = max + 1;
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+function bindEvents() {
+	bindMakeNumberEvent();
+	bindShowAnswerEvent();
+	bindSelectBoxEvent();
+}
+function bindMakeNumberEvent() {
+	// 숫자 만들기 버튼 클릭 이벤트
+	$("#refresh").on('click', function () {
+		initializeNumbers();
+	});
+}
+
+function calculateAnswer() {
+	var answer = 0;
+
+	$('.operand').each(function () {
+		answer += Number($(this).text());
+	});
+	return answer;
+}
+function bindShowAnswerEvent() {
+	$('#show-answer').on('click', function () {
+		var answer = calculateAnswer();
+		var answers = $("#answers");
+		answers.empty();
+		answers.append("<span class='answer'>" + answer + "</span>");
+
+		$(".answer").fadeIn("slow").css("display", "inline-block");
+	});
+}
+
+function bindSelectBoxEvent() {
+	var selectBox = $('#count-select');
+
+	selectBox.dropdown();
+	selectBox.change(function () {
+		initializeNumbers();
+	});
+}
+
+
